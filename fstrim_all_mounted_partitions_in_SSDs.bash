aa4be0436713b599875beb4869128321ea487bfb
Mueem <rusafkakhilji@gmail.com>
2026-09-01 20:48:55 +0500

    Refactor fstrim script to check process before running

--- fstrim_all_mounted_partitions_in_SSDs.bash
+++ fstrim_all_mounted_partitions_in_SSDs.bash
@@ -1,15 +1,18 @@
 #!/usr/bin/bash
 base_path="/.Files used by personal scripts/files used by crontab's daily SSD fstrim operation"
 run_if_date_file_dosent_exist(){
-    /usr/bin/fstrim --all
-	umask 0
-	touch "${base_path}/$1" #adding 
-	rm -f "${base_path}/$2" #removing previous date
+        /usr/bin/fstrim --all
+        umask 0
+        touch "${base_path}/$1" #adding 
+        rm -f "${base_path}/$2" #removing previous date
 }
 check_if_date_exists() {
 current_date=$(date "+%A")
 if [[ ! -e "${base_path}/${current_date}" ]]; then
-	run_if_date_file_dosent_exist "${current_date}" "$(date --date "1 day ago" "+%A")"	
+        if [[ ! $("/.scripts_for_personal_use/shared_scripts fstrim/check_if_process_exist.bash") ]]; then
+                run_if_date_file_dosent_exist "${current_date}" "$(date --date "1 day ago" "+%A")"      
+        fi
 fi
 }
 check_if_date_exists
+

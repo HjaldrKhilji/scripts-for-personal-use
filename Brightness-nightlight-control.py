4182747da7ba9f185d064ab3e52545184eefabc6
Mueem <rusafkakhilji@gmail.com>
2026-09-06 01:01:38 +0500

    Add brightness and nightlight control GUI
    This script provides a GUI for controlling nightlight and brightness settings using Tkinter.

---
+++ Brightness-nightlight-control.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+from tkinter import *
+import tkinter as tk
+import os
+def run_command():
+
+    os.system(f"gammastep -P -O {nightlight.get()} -b {brightness.get()/100} ")
+
+
+master = Tk()
+nightlight_label = tk.Label(master, text="nightlight (lower is more nightlight)")
+nightlight_label.pack()
+nightlight = Scale(master, from_=1000, to=25000, length=1000,tickinterval=1500,orient=HORIZONTAL)
+nightlight.set(4500)
+nightlight.pack()
+
+
+brightness_label = tk.Label(master, text="brightness (lower is darker)")
+brightness_label.pack()
+brightness = Scale(master, from_=10, to=100,length=600, tickinterval=10, orient=HORIZONTAL)
+brightness.set(20)
+brightness.pack()
+Button(master, text='apply', command=run_command).pack()
+mainloop()
+
+#to install tkinter, buld python with the "tk" USE flag

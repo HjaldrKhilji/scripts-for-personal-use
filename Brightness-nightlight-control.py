a9d0cdf5470d3e5ff82e9842ee669343c4ad251f
Mueem <rusafkakhilji@gmail.com>
2026-09-07 20:20:04 +0500

    Refactor brightness-nightlight control script
    Refactor brightness and nightlight control logic to improve functionality and readability. Added independent session updates and improved command execution.

--- Brightness-nightlight-control.py
+++ Brightness-nightlight-control.py
@@ -1,46 +1,48 @@
-Ever hate changing brightness and nightlight in different desktop environments, differently, or not being able to change it in some at all? The gammastep cli utility is there to help (just as it helped me)! If you hate the lack of gui then you should use the simple python script I wrote for it:
-#!/usr/bin/python
-
+#!/usr/bin/env python
 from tkinter import *
-
 import tkinter as tk
-
 import os
 
-#note the gui isnt pretty but that's the point, you are supposed to tune it yourself and share it with others who may find the extra eye candy useful, this script is the bare minimum required with zero bloat (other than python XD)
 
-def run_command():
+base_working_dir='/.Files used by personal scripts'
+base_working_dir_for_helper_scripts=f"{base_working_dir}/helper_scripts"
+working_dir_for_this_script=f"{base_working_dir_for_helper_scripts}/brightness_scripts"
+print(f"working dir for this script: {working_dir_for_this_script}")
 
-os.system(f"gammastep -P -O {nightlight.get()} -b {brightness.get()/100} ")
+
+
+
+def update_session_independently(nightlight_value, brightness_value):
+        with open(f"{working_dir_for_this_script}/BRIGHTNESS_service_command", "w+") as f:
+                f.write(f"#!/sbin/openrc-run \ngammastep -P -O {nightlight_value} -b {brightness_value} \n")
+def update_settings(nightlight_value, brightness_value):
+        os.system(f"gammastep -P -O {nightlight_value} -b {brightness_value} ")
+        update_session_independently(nightlight_value, brightness_value)
+
+def format_brightness(brightness_value):
+        return int(brightness_value)/100
+
+def update_nightlight(nightlight_value):
+        update_settings(nightlight_value, format_brightness(brightness.get()))
+def update_brightness(brightness_value):
+        update_settings(nightlight.get(), format_brightness(brightness_value))
+
+
+
 
 master = Tk()
-
 nightlight_label = tk.Label(master, text="nightlight (lower is more nightlight)")
-
 nightlight_label.pack()
-
-nightlight = Scale(master, from_=1000, to=25000, length=1000,tickinterval=1500,orient=HORIZONTAL)
-
+nightlight = Scale(master, command=update_nightlight, from_=1000, to=25000, length=1000,tickinterval=1500,orient=HORIZONTAL)
 nightlight.set(4500)
-
 nightlight.pack()
 
+
 brightness_label = tk.Label(master, text="brightness (lower is darker)")
-
 brightness_label.pack()
-
-brightness = Scale(master, from_=10, to=100,length=600, tickinterval=10, orient=HORIZONTAL)
-
+brightness = Scale(master, command=update_brightness, from_=10, to=100,length=600, tickinterval=10, orient=HORIZONTAL)
 brightness.set(20)
-
 brightness.pack()
-
-Button(master, text='apply', command=run_command).pack()
-
 mainloop()
 
-#in gentoo:
-
 #to install tkinter, buld python with the "tk" USE flag
-
-#basically, pip intall tkinter or something will probably not suffice on any distrobution, and the gentoo example should make that clear

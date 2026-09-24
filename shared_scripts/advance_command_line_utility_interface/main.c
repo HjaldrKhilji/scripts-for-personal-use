960570dea59c34b51833e8f88b20139db3043263
Mueem <rusafkakhilji@gmail.com>
2026-09-24 18:18:12 +0500

    Fix errors and improve command line argument handling

--- shared_scripts/advance_command_line_utility_interface/main.c
+++ shared_scripts/advance_command_line_utility_interface/main.c
@@ -8,9 +8,10 @@
 
 #include<vector>
 
+#include<iostream>
 namespace errors {
-  void report_and_throw(std::string error) {
-    std::println(std::cerr, error, option[0]);
+  void report_and_throw(const std::string error) {
+    std::println(std::cerr, "{}",error);
     throw std::string {
       error
     };
@@ -35,39 +36,93 @@ namespace read_options {
     input_ops read(std::string_view options);
     void read_more_ops(input_ops & older_ops, std::string_view options) {
       input_ops more_options = read(options.subview(1));
-      result.insert_range(std::back_inserter(older_ops), more_options);
+      older_ops.append_range(more_options);
     }
     input_ops read_args(command_line_option option, std::string_view options) {
-      if (option.lenght() == 0) {
+      if (options.length() == 0) {
         return input_ops {};
       }
       input_ops result {};
       std::string_view args = options.subview(options.find_first_of(' '));
       args = args.subview(0, options.find_first_of(' '));
-      result.push_back(input_ops {
+      result.push_back(options_and_option_args {
         option,
         args
-      });
-      read_more_ops_in_single_string(result, options);
+      } );
+      read_more_ops(result, options);
       return result;
     }
     input_ops read(std::string_view options) {
-      if (option.lenght() == 0) {
+      if (options.length() == 0) {
         return input_ops {};
       }
-      switch (option[0]) {
+      switch (options[0]) {
       case 'r':
         return read_args(command_line_option::remove_if_any_prefix, options);
       default:
-        errors: report_and_throw_invalid_option_error();
+	errors::report_and_throw_invalid_option_error();
       }
     }
   };
   namespace for_multiple_strings {
     input_ops read_interface(int argc, char * argv[]);
     void read_more_ops(input_ops & older_ops, int argc, char * argv[]) {
-      input_ops more_options = read(argc - 2, argv + 2);
-      result.insert_range(std::back_inserter(older_ops), more_options);
+      input_ops more_options = read_interface(argc - 2, argv + 2);
+      older_ops.append_range(more_options);
+    }
+    input_ops read_args(command_line_option option, int argc, char * argv[]) {
+      if (std::strlen(argv[0]) == 0) {
+        return input_ops {};
+      }
+      input_ops result {};
+      std::string_view argument {};
+      if (argc != 0) {
+        std::size_t index;
+        for (index = 1; index < argc && argv[index][0] == '-'; index++);
+        if (index + 1 != argc) {
+          argument = std::string_view {
+            argv[1]
+          };
+        }
+      }
+      result.push_back( options_and_option_args{
+        option,
+        argument
+      });
+      read_more_ops(result, argc, argv);
+      return result;
+    }
+    input_ops read(int argc, char * argv[]) {
+      if (std::strlen(argv[0]) == 0) {
+        return input_ops {};
+      }
+      switch (argv[0][0]) {
+      case 'r':
+        return read_args(command_line_option::remove_if_any_prefix, argc, argv);
+      default:
+	errors::report_and_throw_invalid_option_error();
+      }
+    }
+  };
+  bool use_single_string_funcs(std::string_view string_in_question) {
+    return string_in_question.find_first_of(' ') != std::string_view::npos;
+  }
+  input_ops for_multiple_strings::read_interface(int argc, char * argv[]) {
+    /*assumes the size of input is atleast 2 and that the first argument is not the program name but rather the proper
+    argument*/
+    if (argv[0][0] == '-') {
+      if (use_single_string_funcs(argv[0])) {
+        return for_multiple_strings::read(argc, argv);
+      } else {
+        return for_single_string::read(std::string_view {
+          argv[0] + 1
+        });
+      }
+    } else {
+	return input_ops{};
+    }
+  }
+};
     }
     input_ops read_args(command_line_option option, int argc, char * argv[]) {
       if (std::strlen(argv[0]) == 0) {
